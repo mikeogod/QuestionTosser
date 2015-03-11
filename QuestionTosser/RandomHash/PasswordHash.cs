e486a4896b964eb84e96014e4109bf7776090329
mikeogod <mikeogod@gmail.com>
2015-03-10 23:06:42 -0400

    Sweet Alert

---
+++ QuestionTosser/RandomHash/PasswordHash.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace QuestionTosser.RandomHash
+{
+    public class PasswordHash
+    {
+        public static string ComputeHash(string plainText,
+                                     string hashAlgorithm,
+                                     byte[] saltBytes)
+        {
+            // If salt is not specified, generate it on the fly.
+            if (saltBytes == null)
+            {
+                saltBytes = (RandomSalt(4, 8));
+            }
+
+            // Convert plain text into a byte array.
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            // Allocate array, which will hold plain text and salt.
+            byte[] plainTextWithSaltBytes =
+                    new byte[plainTextBytes.Length + saltBytes.Length];
+
+            // Copy plain text bytes into resulting array.
+            for (int i = 0; i < plainTextBytes.Length; i++)
+                plainTextWithSaltBytes[i] = plainTextBytes[i];
+
+            // Append salt bytes to the resulting array.
+            for (int i = 0; i < saltBytes.Length; i++)
+                plainTextWithSaltBytes[plainTextBytes.Length + i] = saltBytes[i];
+
+            // Because we support multiple hashing algorithms, we must define
+            // hash object as a common (abstract) base class. We will specify the
+            // actual hashing algorithm class later during object creation.
+            HashAlgorithm hash;
+
+            // Make sure hashing algorithm name is specified.
+            if (hashAlgorithm == null)
+                hashAlgorithm = "";
+
+            // Initialize appropriate hashing algorithm class.
+            switch (hashAlgorithm.ToUpper())
+            {
+                case "SHA1":
+                    hash = new SHA1Managed();
+                    break;
+
+                case "SHA256":
+                    hash = new SHA256Managed();
+                    break;
+
+                case "SHA384":
+                    hash = new SHA384Managed();
+                    break;
+
+                case "SHA512":
+                    hash = new SHA512Managed();
+                    break;
+
+                default:
+                    hash = new MD5CryptoServiceProvider();
+                    break;
+            }
+
+            // Compute hash value of our plain text with appended salt.
+            byte[] hashBytes = hash.ComputeHash(plainTextWithSaltBytes);
+
+            // Convert result into a base64-encoded string.
+            string hashValue = Convert.ToBase64String(hashBytes);
+
+            // Return the result.
+            return hashValue;
+        }
+
+        public static byte[] RandomSalt(int minLen, int maxLen)
+        {
+            // Define min and max salt sizes.
+            int minSaltSize = minLen;
+            int maxSaltSize = maxLen;
+
+            // Generate a random number for the size of the salt.
+            Random random = new Random();
+            int saltSize = random.Next(minSaltSize, maxSaltSize);
+
+            // Allocate a byte array, which will hold the salt.
+            byte[] saltBytes = new byte[saltSize];
+
+            // Initialize a random number generator.
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+            // Fill the salt with cryptographically strong byte values.
+            rng.GetNonZeroBytes(saltBytes);
+
+            return saltBytes;
+        }
+    }
+}
---
+++ QuestionTosser/Scripts/main.js
@@ -0,0 +1,51 @@
+﻿var QuestionTosser = QuestionTosser || {};
+QuestionTosser.HideAll = function () {
+    $("#question-panel, #professor-panel, #student-panel, #questions-panel, #student-login-panel,"+ 
+        "#professor-login-panel, #register-panel, #about-panel, #logout-panel").hide();
+};
+QuestionTosser.AnonymousPage = function () {
+    QuestionTosser.HideAll();
+    $("#about-panel").show();
+    $("#student-login-panel").show();
+    $("#professor-login-panel").show();
+    $("#register-panel").show();
+};
+QuestionTosser.StudentPage = function () {
+    QuestionTosser.HideAll();
+    $("#student-panel").show();
+    $("#logout-panel").show();
+};
+QuestionTosser.ProfessorPage = function () {
+    QuestionTosser.HideAll();
+    $("#professor-panel").show();
+    $("#professor-panel input[name='end-class']").hide();
+    $("#logout-panel").show();
+};
+QuestionTosser.JoinSucceed = function () {
+    $("#student-panel input[name='code']").hide();
+    $("#student-panel input[name='submit']").hide();
+    $("#question-panel").show();
+};
+QuestionTosser.StartSucceed = function () {
+    $("#professor-panel input[name='classname']").hide();
+    $("#professor-panel input[name='code']").hide();
+    $("#professor-panel input[name='start-class']").hide();
+    $("#professor-panel input[name='end-class']").show();
+    $("#questions-panel").show();
+};
+QuestionTosser.EndSucceed = function () {
+    $("#professor-panel input[name='classname']").show();
+    $("#professor-panel input[name='code']").show();
+    $("#professor-panel input[name='start-class']").show();
+    $("#professor-panel input[name='end-class']").hide();
+    $("#questions-panel").hide();
+};
+QuestionTosser.ClearLoginRegisterFields = function () {
+    $("#register-panel input[type='text']").val("");
+    $("#register-panel input[type='password']").val("");
+    $("#student-login-panel input[type='text']").val("");
+    $("#student-login-panel input[type='password']").val("");
+    $("#professor-login-panel input[type='text']").val("");
+    $("#professor-login-panel input[type='password']").val("");
+};
+
