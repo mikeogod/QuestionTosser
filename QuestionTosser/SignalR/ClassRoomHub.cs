8a646b90ac5c987edaa5bcd9d385d0c87781db4b
Chenrui26 <chenrui1993@gmail.com>
2015-04-19 20:14:44 -0400

    Commit message

--- QuestionTosser/SignalR/ClassRoomHub.cs
+++ QuestionTosser/SignalR/ClassRoomHub.cs
@@ -13,6 +13,7 @@ namespace SignalRChat
 {
     public class ClassRoomHub : Hub
     {
+        HashSet<string> badWords;
         public override Task OnConnected()
         {
             return base.OnConnected();
@@ -74,15 +75,56 @@ namespace SignalRChat
         public void Toss(string question, string profConnID, string userName, bool anonymous=false)
         {
 
+            badWords = new HashSet<string>();
+
+            badWords.Add("ass");
+                badWords.Add("asshole");
+                badWords.Add("ass hole");
+            badWords.Add("bitch");
+            badWords.Add("shit");
+            badWords.Add("crap");
+            badWords.Add("damn");
+            badWords.Add("fuck");
+                badWords.Add("fucker");
+                badWords.Add("fucking");
+                badWords.Add("motherfucker");
+            badWords.Add("hell");
+            badWords.Add("suck");
+                badWords.Add("sucks");
+                badWords.Add("sucker");
+            badWords.Add("wtf");
+
+            string[] words_array = question.Split(' ');
+
+            foreach (string word in badWords)
+            {
+                for (int i = 0; i < words_array.Length; i++)
+                {
+                    bool if_equal = false;
+                    if_equal = words_array[i].Equals(word, StringComparison.OrdinalIgnoreCase);
+                    if (if_equal == true)
+                    {
+                        words_array[i] = "******";
+                    }
+                }
+            }
+
+            question = "";
+            for (int j = 0; j < words_array.Length; j++)
+            {
+                string new_word = words_array[j] + " ";
+                question += new_word;
+            }
+            System.Diagnostics.Debug.WriteLine(question);
             if (anonymous)
             {
                 userName = "Anonymous";
             }
-            if (question == "" || profConnID=="" || userName=="")
+            if (question == "" || profConnID == "" || userName == "")
             {
                 return;
             }
-            Clients.Client(profConnID).postQuestion(userName+" asked: "+question);
+            Clients.Client(profConnID).postQuestion(userName + " asked: " + question);
         }
 
         [HubMethodName("DisconnectClient")]
